2acd1df14487e91f6a61dbe7fbfd24768b329ffa
Wenjun Xu <906626481@qq.com>
2021-11-15 21:15:52 +0800

    chore: add base path

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'dumi';
 
+const BASE_URL = '/dive-into-react/';
 // more config: https://d.umijs.org/config
 export default defineConfig({
   mode: 'site',
   title: '读懂 React',
-  logo: '/avatar.jpg',
-  favicon: '/avatar.jpg',
+  logo: `${BASE_URL}avatar.jpg`,
+  favicon: `${BASE_URL}avatar.jpg`,
+  base: BASE_URL,
+  publicPath: BASE_URL,
   locales: [['zh-CN', '中文']],
   navs: [
     null,
